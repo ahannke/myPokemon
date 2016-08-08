fee89d5a3e931027b74b2881025c078d594ace73
ahannke <ahannke@gmail.com>
2016-08-08 09:53:27 +0200

    project myPokemon created

---
+++ src/main/java/it/morfoza/MyPokemonMain.java
@@ -0,0 +1,11 @@
+package it.morfoza;
+
+/**
+ * Created by ahann on 08/08/2016.
+ */
+public class MyPokemonMain {
+
+    public static void main(String[] args){
+        Pokemon pokemon = new Pokemon();
+    }
+}
